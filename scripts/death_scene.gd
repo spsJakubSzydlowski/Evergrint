c8719ad95d06eb6f4989a66b4a30c4b32126be74
kubki <szydlowskijakub@sps-karvina.cz>
2026-01-02 18:25:32 +0100

    Create code for respawn

---
+++ scripts/death_scene.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	visible = false
+	Signals.player_died.connect(on_player_died)
+	
+func on_player_died():
+	visible = true
+	var tween = create_tween()
+	tween.tween_interval(5)
+	tween.tween_callback(respawn_player)
+
+func respawn_player():
+	var player = get_tree().get_first_node_in_group("Player")
+	if player:
+		player.respawn()
+		
+	visible = false
