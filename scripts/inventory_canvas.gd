f0b0a95813c2a957fb833125a532395b6715bd52
kubki <szydlowskijakub@sps-karvina.cz>
2026-01-03 14:21:15 +0100

    Inventory UI

--- scripts/inventory_canvas.gd
+++ scripts/inventory_canvas.gd
@@ -4,11 +4,15 @@ signal item_equipped(item_id)
 
 @onready var hotbar_container: HBoxContainer = $hotbar
 @onready var health_bar: TextureProgressBar = $health_bar
+@onready var inventory_container: GridContainer = $inventory
 
 var slot_scene = preload("res://scenes/UI/inventory_slot.tscn")
 var active_slot_index = 0
 
 var hotbar_slots = 10
+var inventory_slots = 50
+
+var is_inventory_open = false
 
 func _ready() -> void:
 	Signals.player_health_changed.connect(update_health_bar)
@@ -22,6 +26,12 @@ func on_inventory_updated():
 	emit_equipped_signal()
 
 func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("open_inventory"):
+		is_inventory_open = !is_inventory_open
+		inventory_container.visible = is_inventory_open
+		hotbar_container.visible = !is_inventory_open
+		refresh_ui()
+
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			active_slot_index = posmod(active_slot_index -1, hotbar_slots)
@@ -34,17 +44,27 @@ func _input(event: InputEvent) -> void:
 
 func refresh_ui():
 	for child in hotbar_container.get_children():
-		child.queue_free()
+		child.free()
+	for child in inventory_container.get_children():
+		child.free()
+
+	if is_inventory_open:
+		for i in range(inventory_slots):
+			create_slot_in(inventory_container, i)
+	else:
+		for i in range(hotbar_slots):
+			create_slot_in(hotbar_container, i)
 		
-	for i in range(hotbar_slots):
-		var slot_data = Inventory.slots[i]
-		var new_slot = slot_scene.instantiate()
-		hotbar_container.add_child(new_slot)
-		
-		new_slot.find_child("SelectionRect").visible = (i == active_slot_index)
-		
-		if slot_data["id"] != "":
-			update_slot_visuals(new_slot, slot_data)
+
+func create_slot_in(container, index):
+	var slot_data = Inventory.slots[index]
+	var new_slot = slot_scene.instantiate()
+	container.add_child(new_slot)
+			
+	new_slot.find_child("SelectionRect").visible = (index == active_slot_index)
+			
+	if slot_data["id"] != "":
+		update_slot_visuals(new_slot, slot_data)
 
 func update_slot_visuals(slot_ui, slot_data):
 	var item_id = slot_data["id"]
@@ -69,7 +89,7 @@ func update_slot_visuals(slot_ui, slot_data):
 	
 	var label = slot_ui.find_child("AmountLabel")
 	label.text = str(amount) if amount > 1 else ""
-
+	
 func emit_equipped_signal():
 	var active_slot_data = Inventory.slots[active_slot_index]
 	item_equipped.emit(active_slot_data["id"])
