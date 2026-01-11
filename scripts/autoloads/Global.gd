85131732ec363f0db3824e2a1d0c29893fdc3761
kubki <szydlowskijakub@sps-karvina.cz>
2026-01-11 14:56:57 +0100

    Create Global.gd
    function transition_to

---
+++ scripts/autoloads/Global.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var world_seed: int = 0
+var player_pos = null
+
+func _ready():
+	randomize()
+	world_seed = randi()
+	print("World seed is: ", world_seed)
+
+var world_scenes = {
+	"surface": "res://scenes/main.tscn",
+	"underground": "res://scenes/underground.tscn"
+}
+
+var current_layer = "surface"
+
+func transition_to(target_layer: String):
+	current_layer = target_layer
+	get_tree().change_scene_to_file(world_scenes[target_layer])
