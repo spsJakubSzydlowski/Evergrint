be6ec163c3944cfab2733461838086e23ca3d83f
kubki <szydlowskijakub@sps-karvina.cz>
2026-01-16 18:11:31 +0100

    Update underground.gd

--- scripts/underground.gd
+++ scripts/underground.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+@onready var object_layer: TileMapLayer = $ObjectLayer
+var center_map_pos = Vector2.ZERO
+
 func _ready() -> void:
 	Global.chunks.clear()
 	
-	DataManager.spawn_player(Global.player_pos)
+	if object_layer:
+		var rect = object_layer.get_used_rect()
+		center_map_pos = rect.position + (rect.size / 2)
+		
+	DataManager.spawn_player(object_layer.map_to_local(center_map_pos))
 	
 	await get_tree().process_frame
