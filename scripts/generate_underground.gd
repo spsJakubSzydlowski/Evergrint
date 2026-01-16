37efc87b85af63b5dcd76a86609c2f66b254bdc7
kubki <szydlowskijakub@sps-karvina.cz>
2026-01-16 18:11:26 +0100

    Update generate_underground.gd

--- scripts/generate_underground.gd
+++ scripts/generate_underground.gd
@@ -9,8 +9,6 @@ var center_map_pos
 
 var occupied_cells = []
 
-var player = null
-
 @export var object_layer: TileMapLayer
 
 func _ready() -> void:
@@ -18,7 +16,6 @@ func _ready() -> void:
 		await get_tree().create_timer(0.1).timeout
 	
 	Signals.block_destroyed.connect(_on_block_destroyed)
-	player = get_tree().get_first_node_in_group("Player")
 	
 	cave_noise.seed = Global.world_seed + 50
 	cave_noise.frequency = 0.05
@@ -28,11 +25,12 @@ func _ready() -> void:
 	river_noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	
 	generate_surface()
-	
 	var rect = get_used_rect()
 	center_map_pos = rect.position + (rect.size / 2)
 	
 	spawn_starting_ladder()
+	generate_blocks()
+	
 	
 func generate_surface():
 	var stone_tiles : Array[Vector2i] = []
@@ -56,41 +54,34 @@ func generate_surface():
 		self.set_cells_terrain_connect(lava_tiles, 0, 3, false)
 
 func spawn_starting_ladder():
-	var sinkhole_pos = center_map_pos + Vector2i(10, 10)
+	var sinkhole_pos = center_map_pos
 
 	object_layer.set_cell(sinkhole_pos, 2, Vector2i(0, 0))
 	
 	occupied_cells.append(sinkhole_pos)
 
-func spawn_blocks(x, y, chunk_node):
-	var val = cave_noise.get_noise_2d(x, y)
+func generate_blocks():
+	print("START GENERATING BLOCKS")
+	var stone_tiles : Array[Vector2i] = []
 	
-	if val > -0.2:
-		var map_pos = Vector2i(x, y)
-		var tile_data = get_cell_tile_data(map_pos)
+	var ladder_radius = 8.0
+	var ladder_pos = center_map_pos
 	
-		if occupied_cells.has(map_pos) or Global.world_changes.get(map_pos) == "removed":
-			return
+	for x in range(world_width):
+		for y in range(world_height):
+			var current_pos = Vector2i(x, y)
 			
-		if tile_data and not tile_data.get_custom_data("water"):
-			#var world_pos = object_layer.map_to_local(map_pos)
+			var dist_from_ladder = Vector2(current_pos).distance_to(Vector2(ladder_pos))
 			
-			object_layer.set_cells_terrain_connect([map_pos], 0, 0, false)
-			#DataManager.spawn_resource("stone_block", world_pos, chunk_node)
+			if dist_from_ladder < ladder_radius:
+				continue
 			
-func _on_chunk_timer_timeout() -> void:
-	var player_world_pos = Global.get_player_world_position()
+			var val = cave_noise.get_noise_2d(x, y)
+			if val > -0.1:
+				stone_tiles.append(Vector2i(x, y))
 	
-	var player_map_pos = local_to_map(player_world_pos)
-	
-	var cx = floor(player_map_pos.x / float(Global.chunk_size))
-	var cy = floor(player_map_pos.y / float(Global.chunk_size))
-	
-	for x in range(cx - Global.render_distance, cx + Global.render_distance + 1):
-		for y in range(cy - Global.render_distance, cy + Global.render_distance + 1):
-			Global.load_chunk(x, y, spawn_blocks, object_layer)
-	
-	Global.unload_chunk(cx, cy)
+	if stone_tiles.size() > 0:
+		object_layer.set_cells_terrain_connect(stone_tiles, 0, 4, false)
 
 func _on_block_destroyed(global_pos):
 	var map_pos = object_layer.local_to_map(global_pos)
