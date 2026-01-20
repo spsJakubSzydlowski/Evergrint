e017d2ad2e24c573c8349993035095153b61b3ed
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-01-20 20:51:57 +0100

    Update death_scene.gd

--- scripts/death_scene.gd
+++ scripts/death_scene.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 func on_player_died():
 	visible = true
 	die_label.visible = true
+
 	var tween = create_tween()
 	tween.tween_interval(5)
 	tween.tween_callback(respawn_player)
@@ -18,6 +19,6 @@ func respawn_player():
 	var player = get_tree().get_first_node_in_group("Player")
 	if player:
 		player.respawn()
-		
+
 	visible = false
 	die_label.visible = false
