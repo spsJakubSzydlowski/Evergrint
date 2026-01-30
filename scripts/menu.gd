d79df2baa33ad7c57c0cdc2d9dd52699f56f22e7
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-01-30 14:06:32 +0100

    Create menu.gd

---
+++ scripts/menu.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+@onready var difficulties: Control = $difficulties
+@onready var start_button: Button = $start_button
+
+func _on_button_pressed() -> void:
+	start_button.visible = false
+	difficulties.visible = true
+	
+func _on_easy_button_pressed() -> void:
+	Global.current_difficulty = Global.Difficulty.EASY
+	start_game()
+
+func _on_hard_button_pressed() -> void:
+	Global.current_difficulty = Global.Difficulty.HARD
+	start_game()
+	
+func start_game():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
