a68b44dd1539a23861fc8fd0b3538e8cd66c628f
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-01-30 17:12:13 +0100

    Update main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -26,7 +26,7 @@ func _ready():
 		DataManager.spawn_item("wooden_hammer", player.global_position, false)
 		DataManager.spawn_item("wooden_bow", player.global_position, false)
 		DataManager.spawn_item("wooden_pickaxe", player.global_position, false)
-		for i in range(100):
+		for i in range(10):
 			DataManager.spawn_item("arrow", player.global_position, false)
 		
 		Global.first_time_generation = false
