f3388b475168da433b1e367a0fbb73de3ea1490b
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-01-31 20:45:10 +0100

    Update underground.gd

--- scripts/underground.gd
+++ scripts/underground.gd
@@ -2,22 +2,31 @@ extends Node2D
 
 @onready var tile_map: TileMapLayer = $TileMapLayer
 @onready var object_layer: TileMapLayer = $ObjectLayer
+var player = null
 var center_map_pos = Vector2.ZERO
 
 func _ready() -> void:
-	Global.chunks.clear()
+	Global.loaded_chunks.clear()
 	
 	Global.current_world_id = "underground"
 	MiningManager.current_tilemap = object_layer
-	
+
 	tile_map.generate()
-	
-	if object_layer:
-		var rect = object_layer.get_used_rect()
-		center_map_pos = rect.position + (rect.size / 2)
-		
-	DataManager.spawn_player(object_layer.map_to_local(center_map_pos))
+
+	spawn_player_at_center()
 	
 	await get_tree().process_frame
 	
-	DataManager.spawn_entity("mole_boss", object_layer.map_to_local(center_map_pos) - Vector2(80, 80))
+	player = get_tree().get_first_node_in_group("Player")
+	
+	DataManager.spawn_entity("mole_boss", player.global_position - Vector2(80, 80))
+
+func _physics_process(_delta: float) -> void:
+	if player:
+		Global.update_chunks(object_layer)
+		
+func spawn_player_at_center():
+	@warning_ignore("integer_division")
+	var center_world_pos = Global.center_world_pos
+	center_map_pos = tile_map.map_to_local(center_world_pos)
+	DataManager.spawn_player(center_world_pos)
