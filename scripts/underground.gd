b6536f4614574b16096700a1d213b30e631ad791
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-01-31 21:42:10 +0100

    Update underground.gd

--- scripts/underground.gd
+++ scripts/underground.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var tile_map: TileMapLayer = $TileMapLayer
 @onready var object_layer: TileMapLayer = $ObjectLayer
 var player = null
-var center_map_pos = Vector2.ZERO
 
 func _ready() -> void:
 	Global.loaded_chunks.clear()
@@ -20,13 +19,17 @@ func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
 	
 	DataManager.spawn_entity("mole_boss", player.global_position - Vector2(80, 80))
+	print(player.global_position)
 
 func _physics_process(_delta: float) -> void:
 	if player:
 		Global.update_chunks(object_layer)
 		
 func spawn_player_at_center():
-	@warning_ignore("integer_division")
 	var center_world_pos = Global.center_world_pos
-	center_map_pos = tile_map.map_to_local(center_world_pos)
-	DataManager.spawn_player(center_world_pos)
+	var center_map_pos = tile_map.map_to_local(center_world_pos)
+	
+	await get_tree().process_frame
+	
+	DataManager.spawn_player(center_map_pos)
+	print(center_map_pos)
