db407346912244ccbf0a8faf3e4d219dcfeb2cf7
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-06 21:49:30 +0100

    Update inventory_slot.gd

--- scripts/inventory_slot.gd
+++ scripts/inventory_slot.gd
@@ -9,18 +9,3 @@ func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			slot_clicked.emit(get_index())
-
-func _make_custom_tooltip(for_text: String) -> Object:
-	if for_text == "":
-		return null
-		
-	var rtl = RichTextLabel.new()
-	rtl.bbcode_enabled = true
-	rtl.fit_content = true
-	rtl.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	rtl.custom_minimum_size = Vector2(150, 0)
-
-	var final_text = for_text
-	
-	rtl.text = final_text
-	return rtl
