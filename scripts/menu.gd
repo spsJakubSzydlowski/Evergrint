3d04cc5119da86ac8eecdd70b0543443abfc5962
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-17 12:57:12 +0100

    Added validation of world name

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -23,9 +23,8 @@ func switch_to_section(target_section: String):
 		sections[section_name].visible = (section_name == target_section)
 
 func _on_name_enter_pressed() -> void:
-	var world_name = name_edit.text.strip_edges()
-	
-	if world_name == "": return
+	print(get_safe_world_name(name_edit.text))
+	var world_name = get_safe_world_name(name_edit.text)
 	
 	Global.world_name = world_name
 	
@@ -35,16 +34,32 @@ func _on_name_enter_pressed() -> void:
 		Signals.play_world.emit(world_name)
 
 func _on_name_edit_text_changed(new_text: String) -> void:
-	if action == "load":
-		name_enter.disabled = false
-		return
-	
 	var worlds = SaveManager.get_all_worlds()
 	name_enter.disabled = false
 
 	for world_name in worlds:
 		if new_text == world_name or new_text == "":
 			name_enter.disabled = true
+
+func get_safe_world_name(input_name: String):
+	var safe_name = input_name.strip_edges()
+	
+	
+	safe_name = safe_name.replace(" ", "").replace("\t", "")
+	
+	safe_name = safe_name.validate_filename()
+	
+	if safe_name == "" or safe_name == "_":
+		safe_name = "Unnamed_World"
+	
+	if safe_name.length() > 32:
+		safe_name = safe_name.substr(0, 32)
+		
+	var regex = RegEx.new()
+	regex.compile("_+")
+	safe_name = regex.sub(safe_name, "_", true)
+	
+	return safe_name
 
 func _on_new_game_button_pressed() -> void:
 	play_click()
@@ -80,7 +95,6 @@ func setup_and_start(difficulty_type, difficulty_name: String):
 	Global.current_difficulty = difficulty_type
 	Global.set_difficulty_mult(difficulty_name)
 	Signals.play_world.emit(Global.world_name)
-	start_game(Global.world_name)
 
 func _play_world_signal(world_name):
 	start_game(world_name)
