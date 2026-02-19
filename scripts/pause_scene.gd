c4d27f34bcfb27b49485e447e2cf00b26eee7296
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-19 15:56:45 +0100

    Create pause_scene.gd

---
+++ scripts/pause_scene.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+var menu = "res://scenes/UI/menu.tscn"
+
+func _ready() -> void:
+	visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		toggle_menu()
+
+func toggle_menu():
+	visible = not visible
+	
+	if visible:
+		get_tree().paused = true
+	else:
+		get_tree().paused = false
+
+func _on_button_mouse_entered(button):
+	if get_node(button).disabled == false:
+		get_node(button).set("theme_override_constants/outline_size", 0)
+
+func _on_button_mouse_exited(button):
+	if get_node(button).disabled == false:
+		get_node(button).set("theme_override_constants/outline_size", 4)
+
+func _on_resume_button_pressed() -> void:
+	toggle_menu()
+
+func _on_savequit_button_pressed() -> void:
+	get_tree().paused = false
+	SceneChanger.change_scene_save_game(menu)
