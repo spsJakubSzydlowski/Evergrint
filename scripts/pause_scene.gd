9af97354f0bc7480bccc611b17a096e41c8bdf4f
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-20 14:30:11 +0100

    FIX: Sound effects

--- scripts/pause_scene.gd
+++ scripts/pause_scene.gd
@@ -9,6 +9,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
 		toggle_menu()
 
+func play_click():
+	AudioManager.play_sfx("menu_click")
+
 func toggle_menu():
 	visible = not visible
 	
@@ -26,8 +29,10 @@ func _on_button_mouse_exited(button):
 		get_node(button).set("theme_override_constants/outline_size", 4)
 
 func _on_resume_button_pressed() -> void:
+	play_click()
 	toggle_menu()
 
 func _on_savequit_button_pressed() -> void:
+	play_click()
 	get_tree().paused = false
 	SceneChanger.change_scene_save_game(menu)
