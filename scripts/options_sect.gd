9f83fa8e57f78ebeff17f35d86617d14401b6d3e
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-20 18:45:48 +0100

    Create options_sect.gd

---
+++ scripts/options_sect.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var done_button: Button = $done_button
+@onready var sound_label: Label = $MarginContainer/VBoxContainer/sound_container/sound_label
+@onready var sound_slider: HSlider = $MarginContainer/VBoxContainer/sound_container/sound_slider
+
+func play_click():
+	AudioManager.play_sfx("menu_click")
+
+func _ready() -> void:
+	var value = SettingsManager.current_settings.get("sound_volume")
+	sound_label.text = "Sound: " + str(int(value)) + "%"
+	sound_slider.value = value
+
+func _on_sound_slider_value_changed(value: float) -> void:
+	sound_label.text = "Sound: " + str(int(value)) + "%"
+	SettingsManager.update_setting("sound_volume", int(value))
+
+func _on_done_button_pressed() -> void:
+	play_click()
+	Signals.switch_to_section.emit("menu")
