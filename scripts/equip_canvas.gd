9ecd7fb9c7706f27c3abae248209c245be894b20
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-24 20:14:25 +0100

    FIX: equip items, add tooltips

--- scripts/equip_canvas.gd
+++ scripts/equip_canvas.gd
@@ -29,14 +29,48 @@ func _on_play_world(_world_name):
 		
 		new_slot.set_meta("equipment_type", type)
 		
-		new_slot.slot_clicked.connect(_on_equipment_slot_clicked)
+		new_slot.mouse_entered.connect(_on_equipment_slot_mouse_entered.bind(new_slot))
+		new_slot.mouse_exited.connect(_on_equipment_slot_mouse_exited)
 		
+		new_slot.slot_clicked.connect(_on_equipment_slot_clicked)
+
+func _on_equipment_slot_mouse_entered(slot_ui):
+	var slot_type = slot_ui.get_meta("equipment_type")
+	var data = Equipment.equipped[slot_type]
+	if data and data.id != "":
+		var item = DataManager.get_item(data.id)
+		Tooltip.display_tooltip(item)
+
+func _on_equipment_slot_mouse_exited():
+	Tooltip.hide_tooltip()
+
 func _on_equipment_slot_clicked(slot_ui):
 	var slot_type = slot_ui.get_meta("equipment_type")
 	
 	if main_ui.first_selected_slot_index != -1:
-		_try_equip_item(slot_type)
+		_try_equip_item(slot_type, slot_ui)
 		
-func _try_equip_item(slot_type):
-	var item_id = Inventory.slots[main_ui.first_selected_slot_index].id
-	print("Trying to equip item: ", str(item_id), " in slot: ", str(slot_type))
+func _try_equip_item(slot_type, equipment_slot_ui):
+	var inventory_index = main_ui.first_selected_slot_index
+	var item_id = Inventory.slots[inventory_index].id
+	
+	var selected_contents = main_ui.selected_slot_contents
+	
+	if selected_contents and Equipment.equipped[slot_type].id == "":
+		Equipment.equipped[slot_type].id = item_id
+		Equipment.equipped[slot_type].amount = Inventory.slots[inventory_index].amount
+		
+		main_ui.update_slot_visuals(equipment_slot_ui, Equipment.equipped[slot_type])
+		main_ui.update_tooltip_data(Equipment.equipped[slot_type], equipment_slot_ui)
+		
+		Inventory.slots[inventory_index] = {"id": "", "amount": 0}
+		
+		if main_ui.selected_slot_contents:
+			main_ui.selected_slot_contents = null
+			
+		main_ui.first_selected_slot_index = -1
+		Tooltip.show_tooltips = true
+		
+		main_ui.refresh_ui()
+	else:
+		print("This slot is not empty!")
