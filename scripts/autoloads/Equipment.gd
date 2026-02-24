6169ea4aa650486938af2dfd1877054f0fe88c9e
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-24 20:14:01 +0100

    Update Equipment.gd

--- scripts/autoloads/Equipment.gd
+++ scripts/autoloads/Equipment.gd
@@ -1,8 +1,8 @@
 extends Node
 
-var equipment = {
-	"head": {"id": "", "amount": 0},
-	"chest": {"id": "", "amount": 0},
-	"legs": {"id": "", "amount": 0},
-	"feet": {"id": "", "amount": 0}
+var equipped = {
+	"head": {"id": ""},
+	"chest": {"id": ""},
+	"legs": {"id": ""},
+	"feet": {"id": ""}
 }
