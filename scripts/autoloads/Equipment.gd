332befeb74e26da785983af2c0f8f76962ae8c3e
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-02-28 11:57:53 +0100

    add default equip and function for armor count

--- scripts/autoloads/Equipment.gd
+++ scripts/autoloads/Equipment.gd
@@ -1,5 +1,12 @@
 extends Node
 
+const equipped_default = {
+	"head": {"id": "", "amount": 0},
+	"chest": {"id": "", "amount": 0},
+	"legs": {"id": "", "amount": 0},
+	"feet": {"id": "", "amount": 0}
+}
+
 var equipped = {
 	"head": {"id": "", "amount": 0},
 	"chest": {"id": "", "amount": 0},
@@ -8,3 +15,13 @@ var equipped = {
 }
 
 const EQUIPMENT_TYPES = ["head", "chest", "legs", "feet"]
+
+func get_armor_amount() -> int:
+	var armor_amount = 0
+	for slot in equipped:
+		var id = equipped[slot]["id"]
+		
+		var armor_stats = DataManager.get_armor_stats(id)
+		var amount = armor_stats.get("armor", 0)
+		armor_amount += amount
+	return armor_amount
