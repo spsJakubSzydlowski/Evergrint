b3f071a48136c4cdf01cfade6c7dd512410f63f9
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-03-03 18:18:48 +0100

    text size tweaks

--- scripts/floating_text.gd
+++ scripts/floating_text.gd
@@ -5,7 +5,7 @@ func setup(text_value: String, color: Color = Color.WHITE):
 	%Label.modulate = color
 	
 	var tween = create_tween()
-	tween.tween_property(self, "scale", Vector2(1.3, 1.3), 0.1)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.1)
 	tween.chain().tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
 	%anim.play("pop_up")
 
