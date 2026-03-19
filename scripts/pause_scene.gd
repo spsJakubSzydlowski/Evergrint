44df222312d5e11668d2873ecdc73f20c55edaab
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-03-19 19:23:04 +0100

    Update pause_scene.gd

--- scripts/pause_scene.gd
+++ scripts/pause_scene.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	visible = false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("escape"):
+	if event.is_action_pressed("pause"):
 		if not options_sect.visible:
 			toggle_menu()
 		else:
