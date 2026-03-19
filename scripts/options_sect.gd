c54a2ca8782d7d750a9fae95798ff098074ea9c4
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-03-19 19:23:01 +0100

    Update options_sect.gd

--- scripts/options_sect.gd
+++ scripts/options_sect.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	sound_slider.value = value
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("escape"):
+	if event.is_action_pressed("pause"):
 		SettingsManager.save_settings()
 
 func _on_sound_slider_value_changed(value: float) -> void:
