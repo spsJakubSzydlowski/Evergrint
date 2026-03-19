132033879912bee7f5d2a3f506c42bdc60bd98fd
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-03-19 19:22:55 +0100

    Update change_control_row.gd

--- scripts/change_control_row.gd
+++ scripts/change_control_row.gd
@@ -5,15 +5,32 @@ extends Button
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
 
+var setting_id
 var setting_name: String
+var button_index: int
 
 func _ready() -> void:
 	
 	setting_label.text = setting_name
+	
+	if button_index < 10:
+		button_text.text = get_mouse_button_text(button_index)
+	else:
+		button_text.text = OS.get_keycode_string(button_index)
+		
+	if button_text.text == "":
+		button_text.text = "<unbound>"
 
 func _on_pressed() -> void:
 	var input_name = await wait_for_input()
 	button_text.text = input_name
+	
+	if button_text.text == "":
+		button_text.text = "<unbound>"
+	
+	Controls.controls[setting_id] = OS.find_keycode_from_string(input_name)
+	
+	print(Controls.controls)
 
 func wait_for_input():
 	anim.play("pulse")
