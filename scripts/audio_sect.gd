36abd9ff3da4c4a1dbe092b0ffe35d1d99fbf038
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-03-21 16:01:30 +0100

    Create audio_sect.gd

---
+++ scripts/audio_sect.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var done_button: Button = $done_button
+@onready var sound_label: Label = $MarginContainer/VBoxContainer/sound_container/sound_label
+@onready var sound_slider: HSlider = $MarginContainer/VBoxContainer/sound_container/sound_slider
+@onready var audio_button: Button = $"../MarginContainer/VBoxContainer/audio_button"
+const BUTTON_HOVERED_STYLE = preload("uid://pufkbrmg787q")
+
+func play_click():
+	AudioManager.play_sfx("menu_click")
+
+func _ready() -> void:
+	var value = SettingsManager.current_settings.get("sound_volume")
+	sound_label.text = "Sound: " + str(int(value)) + "%"
+	sound_slider.value = value
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		SettingsManager.save_settings()
+
+func _on_sound_slider_value_changed(value: float) -> void:
+	sound_label.text = "Sound: " + str(int(value)) + "%"
+	SettingsManager.update_setting("sound_volume", int(value))
+	
+	if sound_slider.has_focus() and int(value) % 6 == 0:
+		AudioManager.play_sfx("slider_ratch")
+
+func _on_done_button_pressed() -> void:
+	play_click()
+	SettingsManager.save_settings()
+	Signals.switch_to_section.emit("menu")
