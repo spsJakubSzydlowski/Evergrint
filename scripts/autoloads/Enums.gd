cc769ec9a4d56f903661c8e01eb9f7c8d960a9c8
spsJakubSzydlowski <szydlowskijakub@sps-karvina.cz>
2026-03-21 18:57:17 +0100

    Update Enums.gd

--- scripts/autoloads/Enums.gd
+++ scripts/autoloads/Enums.gd
@@ -3,3 +3,6 @@ extends Node
 enum PartsOfDay {DAWN, DAY, DUSK, NIGHT}
 
 enum Biomes {FOREST, DESERT, ICE}
+
+enum MenuActions {NONE, CREATE, LOAD, ARE_YOU_SURE, RESET_BINDS}
+var current_menu_action = MenuActions.NONE
